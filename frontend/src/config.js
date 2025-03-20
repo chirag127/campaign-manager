dac1c7f677e35424353438cd3e87901798aa9f2b
Chirag <76880977+chirag127@users.noreply.github.com>
2025-03-20 17:33:55 +0530

    Update API URL in frontend configuration to point to the production server

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,8 +1,8 @@
 // API configuration
 // export const API_URL = "http://localhost:5000";
 
-export const API_URL = "http://192.168.31.232:5000"; // Use your backend server IP and port
-// export const API_URL = "https://sketch2artai.onrender.com/api";
+// export const API_URL = "http://192.168.31.232:5000"; // Use your backend server IP and port
+export const API_URL = "https://campaign-manager-2dj9.onrender.com";
 
 // Platform configuration
 export const PLATFORMS = [
